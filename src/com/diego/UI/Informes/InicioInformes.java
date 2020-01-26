342c59783d1161b8ad4c07d56e6a3b13220ecd3d
Diego <garciadiego769@gmail.com>
2020-01-26 13:27:29 +0100

    Menú Informes

---
+++ src/com/diego/UI/Informes/InicioInformes.java
@@ -0,0 +1,18 @@
+package com.diego.UI.Informes;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InicioInformes extends javax.swing.JFrame{
+    private JPanel panel;
+    private JTextField fechaDesdeTxt;
+    private JTextField fechaHastaTxt;
+    private JLabel fechaDesdelbl;
+    private JLabel fechaHastalbl;
+    private JButton generarInformesbtn;
+
+    public InicioInformes() throws HeadlessException {
+        add(panel);
+        
+    }
+}
